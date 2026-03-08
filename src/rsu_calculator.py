e4549bb6ae6fb2f7e590fb8e63927bfc5ea347ab
Michele Ceccacci <michelececcacci1@gmail.com>
2026-03-08 17:48:14 +0000

    add gains as well

--- src/rsu_calculator.py
+++ src/rsu_calculator.py
@@ -26,8 +26,10 @@ def main() -> int:
             total_cost_basis_eur += transaction.cost_basis / eur_to_usd_buy_rate
         print(f"Total cost basis (USD): {total_cost_basis_usd}")
         print(f"Total proceeds (USD): {total_proceeds_usd}")
+        print(f"Total gains (USD): {total_proceeds_usd - total_cost_basis_usd}")
         print(f"Total cost basis (EUR): {total_cost_basis_eur}")
         print(f"Total proceeds (EUR): {total_proceeds_eur}")
+        print(f"Total gains (EUR): {total_proceeds_eur - total_cost_basis_eur}")
         return 0
     except Exception as e:
         print(f"Error: {e}")
